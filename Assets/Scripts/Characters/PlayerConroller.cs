71772a5fba9c953d73e5e7ba047750c9ca626140
Fabio Augusto <layker191@gmail.com>
2020-02-27 10:55:31 -0300

    NavMeshAgent basic move

---
+++ Assets/Scripts/Characters/PlayerConroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class PlayerConroller : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Camera cam;
+    public LayerMask walkableLayer;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1)){
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit Hit;
+
+            if (Physics.Raycast(ray, out Hit, Mathf.Infinity, walkableLayer))
+            {
+                agent.SetDestination(Hit.point);
+            }
+        }
+    }
+}
