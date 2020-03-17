07297e95d64e11bae4b7bec58b8f23939a98f1dd
Fabio Augusto <layker191@gmail.com>
2020-03-17 11:02:19 -0300

    Using events

--- Assets/Scripts/Characters/CreepController.cs
+++ Assets/Scripts/Characters/CreepController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using NavGame.Core;
+using NavGame.Managers;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class CreepController : CombatGameObject
@@ -20,6 +21,8 @@ public class CreepController : CombatGameObject
         {
             finalTarget = obj.GetComponent<DamageableGameObject>();
         }
+
+        onAttackHit += PlayEffects;
     }
 
     protected override void Update()
@@ -42,5 +45,10 @@ public class CreepController : CombatGameObject
         {
             agent.SetDestination(finalTarget.transform.position);
         }
+    }
+
+    void PlayEffects(Vector3 position)
+    {
+        AudioManager.instance.Play("enemy-hit", position);
     }
 }
--- Assets/Scripts/NavGame/Core/CombatGameObject.cs
+++ Assets/Scripts/NavGame/Core/CombatGameObject.cs
@@ -10,6 +10,7 @@ namespace NavGame.Core
 
         float cooldown = 0f;
 
+        public OnAttackHitEvent onAttackHit;
 
         protected virtual void Update()
         {
@@ -22,7 +23,10 @@ namespace NavGame.Core
             {
                 cooldown = 1f / stats.attackSpeed;
                 target.TakeDamage(stats.damage);
-                AudioManager.instance.Play("enemy-hit", target.transform.position);
+                if (onAttackHit != null)
+                {
+                    onAttackHit(target.transform.position);
+                }
             }
         }
 
