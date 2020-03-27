975e1ebc0b6bf1862d4eaace3c047e5e8fed2184
Fabio Augusto <layker191@gmail.com>
2020-03-26 23:25:37 -0300

    Ofense and defense stats

--- Assets/Scripts/NavGame/Core/CombatGameObject.cs
+++ Assets/Scripts/NavGame/Core/CombatGameObject.cs
@@ -7,6 +7,7 @@ namespace NavGame.Core
 {
     public class CombatGameObject : DamageableGameObject
     {
+        public OfenseStats ofenseStats;
 
         float cooldown = 0f;
 
@@ -21,8 +22,8 @@ namespace NavGame.Core
         {
             if (cooldown <= 0f)
             {
-                cooldown = 1f / stats.attackSpeed;
-                target.TakeDamage(stats.damage);
+                cooldown = 1f / ofenseStats.attackSpeed;
+                target.TakeDamage(ofenseStats.damage);
                 if (onAttackHit != null)
                 {
                     onAttackHit(target.transform.position);
